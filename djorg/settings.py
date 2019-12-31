fac5558ed504eb11c8bf9bac32cc9b8ceb03a358
antilou86 <antilou86@gmail.com>
2019-12-31 12:36:22 -0500

    day 1 MVP complete

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vqc=ktgk=02cjm=90ed@%0c!-lu&kgkrm8!3vu2&5x1g1-g6$)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
